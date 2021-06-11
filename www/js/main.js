527b2c0dc9e117b16aa58fdfa40340f3ae3c4734
unknown <31013@fiecalunos.lan>
2021-06-11 18:10:27 -0300

    new app

---
+++ www/js/main.js
@@ -0,0 +1,13 @@
+// ----------- Slider Menu ----------
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 4000
+  });
+// ----------------------------------
+
+// ---------- Modal Nova Vaga ---------
